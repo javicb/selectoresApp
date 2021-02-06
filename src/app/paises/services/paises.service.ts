8dff1ed3e90d925b2430513610b88e684f006b59
Javi <javi.cb@gmail.com>
2021-02-06 19:10:13 +0100

    selector regiones

--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -5,5 +5,11 @@ import { Injectable } from '@angular/core';
 })
 export class PaisesService {
 
+  private regions: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+  get regiones(): string[] {
+    return [...this.regions];
+  }
+
   constructor() { }
 }
