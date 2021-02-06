f762d3cec283b7a8de46245f47d55e1d2442d14c
Javi <javi.cb@gmail.com>
2021-02-06 18:46:28 +0100

    Formulario reactivo - primer selector

--- src/app/paises/paises.module.ts
+++ src/app/paises/paises.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { PaisesRoutingModule } from './paises-routing.module';
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [SelectorPageComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     PaisesRoutingModule
   ]
 })
