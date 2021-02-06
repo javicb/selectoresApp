4cd5f9ce4d8bfba347b94048b890474321dff695
Javi <javi.cb@gmail.com>
2021-02-06 19:34:25 +0100

    segundo selector anidado

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Pais } from '../../interfaces/paises.interface';
 import { PaisesService } from '../../services/paises.service';
 
 @Component({
@@ -11,16 +12,27 @@ import { PaisesService } from '../../services/paises.service';
 export class SelectorPageComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
-    region: ['', Validators.required]
+    region: ['', Validators.required],
+    pais: ['', Validators.required]
   });
 
   // llenar selectores
   regiones: string[] = [];
+  paises: Pais[] = [];
 
   constructor(private fb: FormBuilder, private paisesService: PaisesService) { }
 
   ngOnInit(): void {
     this.regiones = this.paisesService.regiones;
+
+    // cuando cambie la region
+    this.miFormulario.get('region')?.valueChanges
+      .subscribe(region => {
+        this.paisesService.getPaisesByRegion(region)
+          .subscribe(paises => {
+            this.paises = paises;
+          });
+      });
   }
 
   guardar(): void {
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Pais } from '../interfaces/paises.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -6,10 +9,16 @@ import { Injectable } from '@angular/core';
 export class PaisesService {
 
   private regions: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  private baseUrl = 'https://restcountries.eu/rest/v2';
 
   get regiones(): string[] {
     return [...this.regions];
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPaisesByRegion(region: string): Observable<Pais[]> {
+    const url = `${this.baseUrl}/region/${region}?fields=alpha3Code;name`;
+    return this.http.get<Pais[]>(url);
+  }
 }
